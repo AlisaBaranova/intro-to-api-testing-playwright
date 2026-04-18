d4ef4175d358841f439c9dfea21aae6beb3aeebf
Alisa Baranova <aliskab@inbox.lv>
2026-04-18 19:06:06 +0300

    feat/ab/TL_12

--- src/dto/OrderDTO.ts
+++ src/dto/OrderDTO.ts
@@ -3,41 +3,41 @@ import { z } from 'zod'
 export class OrderDTO {
   status: string
   courierId: number
-  customName: string
-  customPhone: string
+  customerName: string
+  customerPhone: string
   comment: string
   id: number
 
   constructor(
     status: string,
     courierId: number,
-    customName: string,
-    customPhone: string,
+    customerName: string,
+    customerPhone: string,
     comment: string,
     id: number,
   ) {
     this.status = status
     this.courierId = courierId
-    this.customName = customName
-    this.customPhone = customPhone
+    this.customerName = customerName
+    this.customerPhone = customerPhone
     this.comment = comment
     this.id = id
   }
+
   static generateDefault(): OrderDTO {
-    const Orderdto = new OrderDTO('OPEN', 0, 'string', 'string', 'string', 0)
-    return Orderdto
+    const dto = new OrderDTO('OPEN', 0, 'string', 'string', 'string', 0)
+
+    return dto
   }
 }
 
 export const OrderSchema = z
   .object({
     status: z.string(),
-    courierID: z.number().nullable(),
-    customName: z.string(),
-    customPhone: z.string(),
+    courierId: z.number().nullable(),
+    customerName: z.string(),
+    customerPhone: z.string(),
     comment: z.string(),
     id: z.number(),
   })
   .strict()
-
-//export const OrderSchemaResponse
